895f13892c9ecef1911f80c8dfcd71130b9c52b8
Akira Shinohara <k017c1067@it-neec.jp>
2019-06-13 01:01:43 +0900

    [new] #106 @1.5h DBをmariadbへ変更

--- src/cocktail/settings.py
+++ src/cocktail/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import pymysql
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'cocktail.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cocktail',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'maria',
+        'PORT': '3306',
     }
 }
 
