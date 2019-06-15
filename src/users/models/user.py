bee86599a550aaa1243ed037ee81085fcf4db459
Norifumi Fujimaki <k017c1103@it-neec.jp>
2019-06-15 15:49:20 +0900

    [chg] 指摘事項の修正

--- src/users/models/user.py
+++ src/users/models/user.py
@@ -7,31 +7,26 @@ from django.contrib.auth.base_user import AbstractBaseUser
 
 class User(AbstractBaseUser, PermissionMixin):
     # 代理キー（UUIDで実現）
-    user_id = models.UUIDField(primary_key=True, default=uuid.4,
+    id = models.UUIDField(primary_key=True, default=uuid.4,
                                editable=false)
-    # アカウント作成日（DBにINSERTしたとき）
+    # アカウント作成日（DBにINSERTしたときで判断）
     created_at = models.DataTimeField(auto_now_add=True)
-    # アカウント更新日（更新がかかったとき）
     update_at = models.DataTimeField(auto_now=True)
-    # パスワード（とりあえず20文字）
+    # とりあえず20文字
     password = models.CharField(max_length=20)
-    # 前回パスワード
     before_password = models.CharField(max_length=20)
-    # メールアドレス
-    email = models.EmailField(max_length=254)
+    email = models.EmailField(max_length=254, unique=True)
     # 組織名または個人名
     name = models.CharField(max_length=50)
-    # 管理者サイトにTrueならいける
     is_staff = models.BooleanField(default=False)
-    # アクティブかどうか
     is_active = models.BooleanField(default=True)
 
     # 標準のBaseUserManagerを使う代わりにUserManagerを使うことを明示
     objects = UserManager()
 
-    # ユーザのユニークなキー
+    # ユーザのユニークなキーを設定
     USERNAME_FIELD = 'user_id'
-    # ユーザモデルのEメールフィールド
+    # ユーザモデルのEメールフィールドの設定
     EMAIL_FIELD = 'email'
     # 入力必須項目。USERNAME_FIELDとpasswordは書かなくてよい（常に必要なため）
     REQUIRED_FIELDS = ['email']
