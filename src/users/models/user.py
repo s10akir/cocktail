f3df1b318b3493a255518f06d84ccb238e02210f
Akira Shinohara <k017c1067@it-neec.jp>
2019-06-16 19:41:58 +0900

    [del] 不要なスクリプトの削除

--- src/users/models/user.py
+++ src/users/models/user.py
@@ -1,17 +1,17 @@
 import uuid
 
 from django.db import models
-from django.contrib.auth.models import PermissionMixin
+from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.base_user import AbstractBaseUser
 
 
-class User(AbstractBaseUser, PermissionMixin):
+class User(AbstractBaseUser, PermissionsMixin):
     # 代理キー（UUIDで実現）
-    id = models.UUIDField(primary_key=True, default=uuid.4,
-                               editable=false)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                               editable=False)
     # アカウント作成日（DBにINSERTしたときで判断）
-    created_at = models.DataTimeField(auto_now_add=True)
-    update_at = models.DataTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
     # とりあえず20文字
     password = models.CharField(max_length=20)
     before_password = models.CharField(max_length=20)
