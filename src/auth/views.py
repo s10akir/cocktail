31f83d1e872ee86458812a811caf1eb704bdbfd6
Wataru Tsutsumi <k017c1055@it-neec.jp>
2019-06-21 00:51:40 +0900

    [add] #109 @0.5h 会員登録用のビューの実装
    それに伴い会員登録用のフォームも実装

--- src/auth/views.py
+++ src/auth/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate
 
-# Create your views here.
+from .forms import SignupForm
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            email = form.cleaned_data.get('email')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(email=email, password=raw_password)
+            return redirect('/')
+    else:
+        form = SignupForm()
+    return render(request, 'signup.html', {'form': form})
