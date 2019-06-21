60e2d42ae3f020c1e8657a902aa2e66f95d2033c
Wataru Tsutsumi <k017c1055@it-neec.jp>
2019-06-21 12:47:33 +0900

    [fix] #104 @0.5h 指摘事項の修正
    import後の空行
    class間の空行
    if文for文内のインデント

--- src/auth/urls.py
+++ src/auth/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
- 
+
 from . import views
- 
+
 
 app_name = 'auth'
- 
+
 urlpatterns = [
-    path('login/', views.Login.as_view(), name = 'login'),  
-    path('signup/', views.signup, name = 'signup'),
+    path('login/', views.Login.as_view(), name='login'),
+    path('signup/', views.signup, name='signup'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('loggedout/', views.loggedout, name='loggedout'),
 ]
