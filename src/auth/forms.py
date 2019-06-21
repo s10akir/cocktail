2963ab74edaa32f614be00c415ac55d56bac5f15
Wataru Tsutsumi <k017c1055@it-neec.jp>
2019-06-21 12:47:33 +0900

    [fix] #104 @0.5h 指摘事項の修正
    import後の空行
    class間の空行
    if文for文内のインデント

--- src/auth/forms.py
+++ src/auth/forms.py
@@ -2,15 +2,18 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 
+
 User = get_user_model()
 
+
 class SignupForm(UserCreationForm):
-    name = forms.CharField(max_length = 50, help_text = '個人名を入力してください')
-    email = forms.EmailField(max_length = 254, help_text = 'メールアドレスを入力してください')
+    name = forms.CharField(max_length=50, help_text='個人名を入力してください')
+    email = forms.EmailField(max_length=254, help_text='メールアドレスを入力してください')
 
     class Meta:
         model = User
         fields = ('name', 'email', 'password1', 'password2')
+
 
 class LoginForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
