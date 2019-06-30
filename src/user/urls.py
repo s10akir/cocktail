e94f0bd01d8db0a098408cd57101187e624ea946
Kenta Houki <k017c1165@it-neec.jp>
2019-07-01 02:29:49 +0900

    [new] #213 @12h ユーザ情報変更機能を実装

--- src/user/urls.py
+++ src/user/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('loggedout/', views.loggedout, name='loggedout'),
+    path('updateInformation/', views.update, name='updateInformation')
 ]
