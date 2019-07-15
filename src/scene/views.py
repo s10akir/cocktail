67b4854ccf0859215c816bc1e63f5fd5d3b72e67
Wataru Tsutsumi <k017c1055@it-neec.jp>
2019-07-16 01:52:28 +0900

    [add] #258 @3h シーンデータをテンプレートへ渡す
    取得したシーンのデータをテンプレートへ渡して表示

--- src/scene/views.py
+++ src/scene/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 
+from .models import Scene
+
 
 def showSceneData(request):
+    if 'id' in request.GET:
+        scene_id = request.GET.get('id')    # GET時のクエリ文字列のidからシーンIDを取得
+        scene = Scene()
+        scene_data = scene.get_scene_data(scene_id)
+
+        if scene_data is not None:
+            data = {'scene_data': scene_data}
+            return render(request, 'show-scene-data.html', data)
+
     return render(request, 'show-scene-data.html')
