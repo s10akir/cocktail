bcd855053011228fc52e0a6fc6c7fde605fd16bb
Wataru Tsutsumi <k017c1055@it-neec.jp>
2019-07-16 02:01:49 +0900

    [add] #258 @2h 非ログイン時のアクセス制限

--- src/scene/views.py
+++ src/scene/views.py
@@ -1,8 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 from .models import Scene
 
 
+@login_required
 def showSceneData(request):
     if 'id' in request.GET:
         scene_id = request.GET.get('id')    # GET時のクエリ文字列のidからシーンIDを取得
