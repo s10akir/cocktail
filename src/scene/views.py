f096941708c3e314469987d564056c57b57048c0
Wataru Tsutsumi <k017c1055@it-neec.jp>
2019-07-16 02:42:26 +0900

    [add] #264 @0.5 非ログイン時のアクセス制限

--- src/scene/views.py
+++ src/scene/views.py
@@ -1,8 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 from .models import SizeTemplate
 
 
+@login_required
 def showSizeTemplates(request):
     size_template = SizeTemplate()
     templates = size_template.get_size_templates()
