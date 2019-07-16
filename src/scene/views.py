491d6e72dda7eed5573ca40f88eeafc5c0a338a9
Wataru Tsutsumi <k017c1055@it-neec.jp>
2019-07-16 13:41:29 +0900

    [fix] #264 変数の命名の変更

--- src/scene/views.py
+++ src/scene/views.py
@@ -8,5 +8,5 @@ from .models import SizeTemplate
 def showSizeTemplates(request):
     size_template = SizeTemplate()
     templates = size_template.get_size_templates()
-    data = {'templates': templates}
-    return render(request, 'show-size-templates.html', data)
+    templates_data = {'templates': templates}
+    return render(request, 'show-size-templates.html', templates_data)
