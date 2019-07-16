d161cb1768507e8b945cf220026052d2ff846041
Norifumi Fujimaki <k017c1103@it-neec.jp>
2019-07-16 13:53:45 +0900

    [chg] "pep8に沿った書き方に変更"

--- src/scene/forms.py
+++ src/scene/forms.py
@@ -1,14 +1,29 @@
 from django import forms
+from django.core.validators import (
+    MaxValueValidator,
+    MinValueValidator
+)
 
 from scene.models.scene import Scene
 
 
 class SceneConfigForm(forms.ModelForm):
     name = forms.CharField(max_length=30, help_text='シーン名を入力してください')
-    width = forms.IntegerField(help_text='シーンサイズの横の大きさを入力してください')
-    hight = forms.IntegerField(help_text='シーンサイズの縦の大きさを入力してください')
-    column_count = forms.IntegerField(help_text='横のグリッド数を入力してください')
-    line_count = forms.IntegerField(help_text='縦のグリッド数を入力してください')
+    # 下記サイトを参考に最大値は横を8192、縦を4320。最小値は横128、縦96。
+    # http://www.quel.jp/etc/monitor-size/
+    width = forms.IntegerField(validators=[MinValueValidator(128),
+                               MaxValueValidator(8192)],
+                               help_text='シーンサイズの横の大きさを入力してください')
+    hight = forms.IntegerField(validators=[MinValueValidator(96),
+                               MaxValueValidator(4320)],
+                               help_text='シーンサイズの縦の大きさを入力してください')
+    # 試験的に最大横グリッド数を100、縦グリッド数を100。
+    column_count = forms.IntegerField(validators=[MinValueValidator(1),
+                                      MaxValueValidator(100)],
+                                      help_text='横のグリッド数を入力してください')
+    line_count = forms.IntegerField(validators=[MinValueValidator(1),
+                                    MaxValueValidator(100)],
+                                    help_text='縦のグリッド数を入力してください')
 
     class Meta():
         model = Scene
