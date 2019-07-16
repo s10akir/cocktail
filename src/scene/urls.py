526268d1c7caf9d230bfb6bf14779b175e8672ce
Norifumi Fujimaki <k017c1103@it-neec.jp>
2019-07-16 14:34:02 +0900

    [chg] "pep8に沿った書き方に変更"

--- src/scene/urls.py
+++ src/scene/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from . import views
+from scene import views
 
 
 app_name = 'scene'
--- src/scene/views.py
+++ src/scene/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import (
+    redirect,
+    render
+)
 from django.contrib.auth.decorators import login_required
 
 from scene.forms import SceneConfigForm
