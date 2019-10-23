8dec6478484f87712f144d1495975734b006fd8c
Wataru Tsutsumi <k017c1055@it-neec.jp>
2019-10-23 14:40:38 +0900

    [add] モジュールデータ用の仮のモデル作成

--- src/scene/models/__init__.py
+++ src/scene/models/__init__.py
@@ -1,2 +1,3 @@
 from scene.models.scene import Scene
 from scene.models.size_template import SizeTemplate
+from scene.models.module_used import Module_used
---
+++ src/scene/models/module_used.py
@@ -0,0 +1,15 @@
+from uuid import uuid4
+
+from django.db import models
+
+
+class Module_used(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    scene_id = models.ForeignKey('scene.Scene',
+                                 on_delete=models.PROTECT,
+                                 db_column='scene_id')
+    module_id = models.CharField(max_length=10)
+    row = models.CharField(max_length=10)
+    column = models.CharField(max_length=10)
+    grid_width = models.CharField(max_length=10)
+    grid_height = models.CharField(max_length=10)
